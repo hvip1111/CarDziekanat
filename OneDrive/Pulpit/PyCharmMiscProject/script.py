5b8cd182972df54f8a80fa08aca10c93ed3af7d0
Łukasz Balewicz <251121@edu.p.lodz.pl>
2025-05-10 14:37:07 +0200

    2. dodany pokuj opłaty,dodane kolizje w pokoju opłaty i pokoju wf

--- OneDrive/Pulpit/PyCharmMiscProject/script.py
+++ OneDrive/Pulpit/PyCharmMiscProject/script.py
@@ -13,31 +13,45 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
 # Wczytanie tła
-background = pygame.image.load("mapa.png")
+background = pygame.image.load("map.png")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
-background1 = pygame.image.load("background1.png")
+background1 = pygame.image.load("payments.png")
 background1 = pygame.transform.scale(background1, (WIDTH, HEIGHT))
-background2 = pygame.image.load("zapisynawf.png")
+background2 = pygame.image.load("PE_enrollment.png")
 background2 = pygame.transform.scale(background2, (WIDTH, HEIGHT))
 
-# Wczytanie maski kolizji
-road_mask = pygame.image.load("maskbackground.png").convert()
+# Wczytanie masek kolizji
+road_mask = pygame.image.load("map_mask.png").convert()
 road_mask = pygame.transform.scale(road_mask, (WIDTH, HEIGHT))
 
-# Wczytanie pojazdu (zachowujemy oryginalny rozmiar 50x50)
-car = pygame.image.load("car.png")
-car = pygame.transform.scale(car, (50, 50))
+payments_mask = pygame.image.load("payments_mask.png").convert()
+payments_mask = pygame.transform.scale(payments_mask, (WIDTH, HEIGHT))
 
-# Ładowanie animacji postaci z większym rozmiarem (75x75)
-PLAYER_WIDTH, PLAYER_HEIGHT = 100, 100  # Nowy rozmiar postaci (150% z 50x50)
+pe_enrollment_mask = pygame.image.load("PE_enrollment_mask.png").convert()
+pe_enrollment_mask = pygame.transform.scale(pe_enrollment_mask, (WIDTH, HEIGHT))
 
-prawo_img = pygame.image.load("prawo.png").convert_alpha()
+# Wczytanie pojazdu
+car_left = pygame.image.load("car_left.png")
+car_left = pygame.transform.scale(car_left, (60, 60))
+car_right = pygame.image.load("car_right.png")
+car_right = pygame.transform.scale(car_right, (60, 60))
+car_up = pygame.image.load("car_up.png")
+car_up = pygame.transform.scale(car_up, (60, 60))
+car_down = pygame.image.load("car_down.png")
+car_down = pygame.transform.scale(car_down, (60, 60))
+
+car_direction = "right"
+
+# Ładowanie postaci
+PLAYER_WIDTH, PLAYER_HEIGHT = 100, 100
+
+prawo_img = pygame.image.load("character_right.png").convert_alpha()
 prawo_img = pygame.transform.scale(prawo_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
-prawo_idzie_img = pygame.image.load("prawo_idzie.png").convert_alpha()
+prawo_idzie_img = pygame.image.load("character_right_move.png").convert_alpha()
 prawo_idzie_img = pygame.transform.scale(prawo_idzie_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
-lewo_img = pygame.image.load("lewo.png").convert_alpha()
+lewo_img = pygame.image.load("character_left.png").convert_alpha()
 lewo_img = pygame.transform.scale(lewo_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
-lewo_idzie_img = pygame.image.load("lewo_idzie.png").convert_alpha()
+lewo_idzie_img = pygame.image.load("character_left_move.png").convert_alpha()
 lewo_idzie_img = pygame.transform.scale(lewo_idzie_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 # Zmienne animacji
@@ -47,8 +61,9 @@ animation_counter = 0
 animation_delay = 10
 current_frame = 0
 
-# Pozycja początkowa i zmienne
-car_x, car_y = 400, 300
+# Pozycje i zmienne
+car_x, car_y = 400, 280
+character_x, character_y = 350, 450  # pozycja postaci
 speed = 5
 buildings = [(100, 0), (730, 225)]
 current_background = background
@@ -56,7 +71,6 @@ inside_building = False
 entry_position = None
 near_building = False
 font = pygame.font.Font(None, 36)
-
 
 def update_animation():
     global animation_counter, current_frame
@@ -68,19 +82,16 @@ def update_animation():
     else:
         current_frame = 0
 
-
 def get_player_image():
     if facing == "prawo":
         return prawo_idzie_img if current_frame else prawo_img
     else:
         return lewo_idzie_img if current_frame else lewo_img
 
-
 def draw_squares():
     if not inside_building:
         pygame.draw.rect(screen, RED, (100, 0, 50, 50))
         pygame.draw.rect(screen, RED, (730, 225, 50, 50))
-
 
 running = True
 while running:
@@ -98,17 +109,16 @@ while running:
                     car_x, car_y = entry_position
             elif event.key == pygame.K_e and near_building and not inside_building:
                 inside_building = True
-                car_x, car_y = WIDTH // 2 - PLAYER_WIDTH // 2, HEIGHT // 2 - PLAYER_HEIGHT // 2  # Centrowanie większej postaci
                 if near_building == (100, 0):
                     current_background = background1
                 elif near_building == (730, 225):
                     current_background = background2
 
     keys = pygame.key.get_pressed()
-    new_x, new_y = car_x, car_y
     is_moving = False
 
     if inside_building:
+        new_x, new_y = character_x, character_y
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             new_x -= speed
             facing = "lewo"
@@ -124,17 +134,34 @@ while running:
             new_y += speed
             is_moving = True
 
-        car_x, car_y = new_x, new_y
+        player_center = (new_x + PLAYER_WIDTH // 2, new_y + PLAYER_HEIGHT // 2)
+
+        if current_background == background1:
+            pixel_color = payments_mask.get_at(player_center)
+            if pixel_color == (255, 255, 255, 255):
+                character_x, character_y = new_x, new_y
+        elif current_background == background2:
+            pixel_color = pe_enrollment_mask.get_at(player_center)
+            if pixel_color == (255, 255, 255, 255):
+                character_x, character_y = new_x, new_y
+        else:
+            character_x, character_y = new_x, new_y
+
         update_animation()
     else:
+        new_x, new_y = car_x, car_y
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             new_x -= speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            car_direction = "left"
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             new_x += speed
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            car_direction = "right"
+        elif keys[pygame.K_UP] or keys[pygame.K_w]:
             new_y -= speed
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            car_direction = "up"
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             new_y += speed
+            car_direction = "down"
 
         if road_mask.get_at((new_x + 25, new_y + 25)) == (255, 255, 255, 255):
             car_x, car_y = new_x, new_y
@@ -149,9 +176,16 @@ while running:
                 break
 
     if inside_building:
-        screen.blit(get_player_image(), (car_x, car_y))
+        screen.blit(get_player_image(), (character_x, character_y))
     else:
-        screen.blit(car, (car_x, car_y))
+        if car_direction == "left":
+            screen.blit(car_left, (car_x, car_y))
+        elif car_direction == "right":
+            screen.blit(car_right, (car_x, car_y))
+        elif car_direction == "up":
+            screen.blit(car_up, (car_x, car_y))
+        elif car_direction == "down":
+            screen.blit(car_down, (car_x, car_y))
 
         if near_building:
             text = font.render("Wciśnij E, aby wejść", True, BLACK)
